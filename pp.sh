1fdb8c83d3e25f6284313153531ae90e3e9f46e5
Elliott Cable <elliott.cable@ahrefs.com>
2023-06-20 19:22:09 +0000

    (- script new) pp.sh diffs output

--- pp.sh
+++ pp.sh
@@ -4,13 +4,24 @@
 #
 #     ./pp.sh --impl test/test_trace_expression.ml
 
+realpath() { echo "$(
+   cd "$(dirname "$1")" || return
+   pwd
+)/$(basename "$1")"; }
+
 type="$1"
 shift
+filename="$(realpath "$1")"
+shift
+
+output="${filename%.ml}.pp.ml"
+
 case "$type" in
 "--impl" | "--intf")
    opam exec --sw=. -- \
-      dune exec --display=quiet test/errors/bin/pp.exe -- "$type" "$@" |
-      ocamlformat "$type" -
+      dune exec --display=quiet test/errors/bin/pp.exe -- "$type" "$filename" "$@" |
+      ocamlformat "$type" - >"$output" &&
+      patdiff "$filename" "$output"
    ;;
 *)
    printf "%s\n" "First argument must be either --impl or --intf" >&2
